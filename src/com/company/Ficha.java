e1ac2e4ac1e30a341afe808e4005daf2edc30fa2
Volkanico <vcekmez15181@alumnes.iesmanacor.cat>
2022-06-01 11:25:56 +0200

    Presentacio variables basiques

--- src/com/company/Ficha.java
+++ src/com/company/Ficha.java
@@ -1,6 +1,14 @@
 package com.company;
 
 public class Ficha {
-    //FICHA DOMINO
+
+    int num1;
+    int num2;
+    boolean isTapat;
+
+    public Ficha (int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
 
 }
--- src/com/company/Tablero.java
+++ src/com/company/Tablero.java
@@ -1,4 +1,22 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Tablero {
+
+    ArrayList<Ficha> fichasTotales;
+
+
+
+    public void initFichas() {
+        for(int i = 0; i <= 6; i++) {
+            for (int j = i; j <= 6; j++){
+                fichasTotales.add(new Ficha(i,j));
+            }
+        }
+    }
+
+
+
+
 }
