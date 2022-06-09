ce8c98adbc7497ff56899739a030e6c1daff8734
Volkanico <vcekmez15181@alumnes.iesmanacor.cat>
2022-06-09 10:44:02 +0200

    InputOutput Jugadors

--- src/com/company/InputOutput.java
+++ src/com/company/InputOutput.java
@@ -1,8 +1,92 @@
 package com.company;
 
-public class InputOutput {
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    Jugador jugador1 = new Jugador("Pepito");
-    Jugador jugador2 = new Jugador("Benancio");
+public class InputOutput {
+    Scanner sc = new Scanner(System.in);
+
+    int decision;
+    String numJugadorParsed;
+    String nomJugador;
+
+    Jugador jugador1 = new Jugador("Jugador 1");
+    Jugador jugador2 = new Jugador("Jugador 2");
+    Jugador jugador3 = new Jugador("Jugador 3");
+    Jugador jugador4 = new Jugador("Jugador 4");
+
+
+    Jugador[] jugadorsDisponibles;
+    Jugador[] jugadorsQueJuguen;
+
+    ArrayList<Jugador> jugadors = new ArrayList<Jugador>();
+
+    public void guardarJugadors () {
+        for (int i = 0; i < jugadors.size(); i++) {
+
+           // numJugadorParsed = String.valueOf(numJugador);
+            nomJugador = "Jugador" + numJugadorParsed;
+
+
+            //jugadors.add();
+        }
+    }
+
+    public void introduction () {
+        System.out.println("¡Bienvenido a Domino!");
+
+        System.out.println("¿Desea jugar individualmente o por equipos?");
+        System.out.println("Pulse 1 para individual");
+        System.out.println("Pulse 2 para equipos");
+        if (decision == 1) {
+            decisionDeJugadores();
+        }
+        if (decision == 2) {
+            decisionDeJugadores();
+        }
+
+
+
+    }
+
+
+
+        public void decisionDeJugadores () {
+            System.out.println("Jugador 1 introduzca su nombre: ");
+            jugador1.setNom(sc.nextLine());
+            System.out.println(jugador1.getNom());
+
+            System.out.println("Jugador 2 introduzca su nombre: ");
+            jugador2.setNom(sc.nextLine());
+            System.out.println(jugador2.getNom());
+
+            System.out.println("¿Participan mas jugadores?");
+            System.out.println("Pulse 1 para SI");
+            System.out.println("Pulse 2 para NO");
+            decision = sc.nextInt();
+            if (decision == 1) {
+                System.out.println("Jugador 3 introduzca su nombre: ");
+                jugador3.setNom(sc.nextLine());
+                System.out.println(jugador3.getNom());
+
+                System.out.println("¿Participan mas jugadores?");
+                System.out.println("Pulse 1 para SI");
+                System.out.println("Pulse 2 para NO");
+                decision = sc.nextInt();
+                if (decision == 1) {
+
+                    System.out.println("Jugador 4 introduzca su nombre: ");
+                    jugador4.setNom(sc.nextLine());
+                    System.out.println(jugador4.getNom());
+                }
+                if (decision == 2) {
+                    //Siguiente metodo
+                }
+            }
+
+            if (decision == 2) {
+                //Siguiente metodo
+            }
+        }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         Tablero tablero = new Tablero();
+        InputOutput inputOutput = new InputOutput();
 
         tablero.initFichas();
         tablero.imprimirFichas();
+        inputOutput.introduction();
+
 
     }
 }
